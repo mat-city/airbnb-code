4ad9cd9f2f698662d259e182515a8f40ddc74250
mat-city <mo.tariq654@gmail.com>
2025-08-23 19:08:40 +0900

    bugg fix

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import random
 #from confluent_kafka import Producer
 import json
 
-st.set_option('server.enableStaticServing', True)
-st.set_option('server.enableCORS', False)
+# st.set_option('server.enableStaticServing', True)
+# st.set_option('server.enableCORS', False)
 
 # Load the model
 with open("best_airbnb_model_more_featuresV4.pkl", "rb") as f:
